742374c811ef2ebe678a922451105b773c87257c
Ashley Bekemeier <bekemeierashley@gmail.com>
2023-07-29 18:29:50 +0200

    Add router children

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CompanionComponent } from './companion/companion.component';
+import { CompanionSelectComponent } from './companion-select/companion-select.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'companion', component: CompanionComponent },
+  {
+    path: '', component: HomeComponent, children: [
+      { path: '', component: CompanionSelectComponent },
+      { path: 'companion', component: CompanionComponent },
+    ]
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
